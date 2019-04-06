67217c50d70ce43d5626457a3e4019307fe55747
davi.seares <davi.bseares@gmail.com>
2019-04-06 02:31:36 -0300

    Subindo o projeto

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="fa fa-home" title="Início" subtitle="Cadastro em React">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema de cadastro desenvolvido em React</p>
+    </Main>
